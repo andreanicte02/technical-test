d7df8b57de192b201af9de32eee392099dd92a88
andreanicte02 <andreanicte02@gmail.com>
2022-02-07 00:59:13 -0600

    odel

---
+++ backend/src/main/java/com/example/backend/model/Establecimiento.java
@@ -0,0 +1,42 @@
+package com.example.backend.model;
+
+public class Establecimiento
+{
+    private int id;
+    private String nombre;
+    private String telefono;
+    private String ubicacion;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getUbicacion() {
+        return ubicacion;
+    }
+
+    public void setUbicacion(String ubicacion) {
+        this.ubicacion = ubicacion;
+    }
+}
